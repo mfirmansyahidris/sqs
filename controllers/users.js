e223aee39ee58b2c6ee392f7e1d29808723acacd
manca_fi <manca.fi@gmail.com>
2019-09-03 14:31:54 +0800

    last init

--- controllers/users.js
+++ controllers/users.js
@@ -2,15 +2,20 @@ var response = require('../res/index')
 var connection = require('../connection/index')
 
 exports.index = function(req, res){
-    connection.query('SELECT * FROM users', function (error, rows, fields){
+    connection.query(`SELECT * FROM users WHERE id = '${req.body.id}'`, function (error, rows, fields){
         if(error){
             console.log(error)
             response.failed({
                 'message' : error    
             }, res)
         }else{
+            if(rows.length < 1){
+                response.notfound({
+                    'message' : 'user not found'
+                }, res)
+            }
             response.ok({
-                'users' : rows
+                'users' : rows[0]
             }, res)
         }
     })
